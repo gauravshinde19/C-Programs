56937d7ad7acad494addfe0e377aad8a14ebaa13
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-04-29 14:42:41 +0530

    Add files via upload
    Program to print primitive data types

---
+++ Basic_Display/printDataTypes.c
@@ -0,0 +1,16 @@
+/* Program to print datatypes */
+
+#include<stdio.h>
+
+int main()
+{
+    int iValue = 10;
+    float fValue = 20.89f;
+    double dValue = 30.0689862;
+    char cValue = 'G';
+
+    printf("iValue : %d\n", iValue);
+    printf("fValue : %.2f\n", fValue);
+    printf("dValue : %f\n", dValue);
+    printf("cValue : %c\n", cValue);
+}
