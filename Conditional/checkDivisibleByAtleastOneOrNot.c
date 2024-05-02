78e049c0132613cf4a63ee08a57eed556dea8cc2
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-02 23:59:07 +0530

    Add files via upload

---
+++ Conditional/checkDivisibleByAtleastOneOrNot.c
@@ -0,0 +1,39 @@
+/* Program to accept integer from user and check whether number is divisible by 3 or 4 or not */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+
+    if (((iNo % 3) == 0) || ((iNo % 4) == 0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if (bRet == true)
+    {
+        printf("Number is divisible by 3 or 4\n");
+    }
+    else
+    {
+        printf("Number is not divisible by 3 or 4\n");
+    }
+
+    return 0;
+}
