c3007ba5cd655e33ee01d068b038c06cad0e3d9e
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-07 00:38:45 +0530

    Create summationEvenDigits.c

---
+++ Digits/summationEvenDigits.c
@@ -0,0 +1,39 @@
+/* Program to acept number from user and perform summation of even digits of the number */
+
+#include<stdio.h>
+
+int SumEvenDigits(int iNo)
+{
+    int iDigit = 0;
+    int iSum = 0;
+    
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if (iDigit % 2 == 0)
+        {
+            iSum = iSum + iDigit;
+        }   
+        iNo = iNo / 10;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number :\n");
+    scanf("%d", &iValue);
+
+    iRet = SumEvenDigits(iValue);
+    printf("Summation of even digits of %d :\n%d", iValue, iRet);
+
+    return 0;
+}
