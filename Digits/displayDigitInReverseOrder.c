4da6605724a22993becc45e5edbed7a1f4b512fa
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-06 22:59:31 +0530

    Create displayDigitInReverseOrder.c

---
+++ Digits/displayDigitInReverseOrder.c
@@ -0,0 +1,32 @@
+/* Program to accept number from user and print digits of the number in reverse order */    // ***
+
+#include<stdio.h>
+
+void DisplayDigits(int iNo)
+{
+    int iDigit = 0;
+    
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d\n", iDigit);
+        iNo = iNo / 10;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :\n");
+    scanf("%d", &iValue);
+
+    DisplayDigits(iValue);
+
+    return 0;
+}
