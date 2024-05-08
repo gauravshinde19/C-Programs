24c08e82aa3e45eacaf4eb6df3119c595368a41a
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-09 00:50:08 +0530

    Create pattern7.c

---
+++ Pattern/pattern7.c
@@ -0,0 +1,31 @@
+/*  
+    Program to accept number from user and display following pattern
+    Input : 5
+    Output : -5   -4   -3   -2   -1       
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+ 
+    for (iCnt = -iNo; iCnt < 0; iCnt++)
+    {
+        printf("%d\t", iCnt);
+        
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
