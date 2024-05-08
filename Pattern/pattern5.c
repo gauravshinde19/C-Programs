09a6e85f8834071dfea34f587b4e38e7e2e85855
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-08 23:25:14 +0530

    Create pattern5.c

---
+++ Pattern/pattern5.c
@@ -0,0 +1,30 @@
+/*  
+    Program to accept number from user and display following pattern
+    Input : 5
+    Output : 1  *  1    2  *  2    3  *  3    4  *  4    5  *  5       
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d\t*\t%d\t", iCnt, iCnt);
+        
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
