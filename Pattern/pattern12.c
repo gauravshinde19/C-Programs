706377d25da0a33a02a5c3f1471239908c74d742
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-10 13:55:10 +0530

    Create pattern12.c

---
+++ Pattern/pattern12.c
@@ -0,0 +1,31 @@
+/*  
+    Program to accept number from user and display following pattern
+    Input : 5
+    Output : A    *   B    *   C    *   D    *   E    *      
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+    
+    for (iCnt = 1, ch = 'A'; iCnt <= iNo; iCnt++, ch++)
+    {
+        printf("%c\t*\t", ch);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
