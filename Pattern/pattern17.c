512048e7d02114063e1b63a5849df81c95bf4d3a
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-11 17:55:22 +0530

    Create pattern17.c

---
+++ Pattern/pattern17.c
@@ -0,0 +1,48 @@
+/*
+    Program to accept number from user and display following pattern
+    Input : rows = 4, columns = 4
+    Output :    *    *    *    *
+                $    $    $    $
+                *    *    *    *
+                $    $    $    $
+*/
+
+#include <stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (i % 2 == 0)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns:\n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
