fb05cb7e925096262fa81428059e2ddb796d038c
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-12 20:26:16 +0530

    Create pattern25.c

---
+++ Pattern/pattern25.c
@@ -0,0 +1,50 @@
+/*
+    Program to accept number from user and display following pattern
+    Input : rows = 4, columns = 4
+    Output :    1            
+                1    2        
+                1    2    3    
+                1    2    3    4
+*/
+
+#include <stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if (iRow != iCol)
+    {
+        printf("Number of rows and columns should be same");
+        return;
+    }
+    
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (i >= j)
+            {
+                printf("%d\t", j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns:\n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
