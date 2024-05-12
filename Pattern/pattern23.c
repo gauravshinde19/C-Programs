1aa6ff5aedc0ace261be9e916097a774b4d7a59c
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-12 20:24:14 +0530

    Create pattern23.c

---
+++ Pattern/pattern23.c
@@ -0,0 +1,58 @@
+/*
+    Program to accept number from user and display following pattern
+    Input : rows = 4, columns = 4
+    Output :    $    @    @    @
+                #    $    @    @
+                #    #    $    @
+                #    #    #    $
+*/
+
+#include <stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if (iRow != iCol)
+    {
+        printf("Number of rows and columns should be same");
+        return;
+    }
+    
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (i > j)          // lower triangle
+            {
+                printf("#\t");
+            }
+            else if (j > i)     // upper triangle
+            {
+                printf("@\t");
+            } 
+            else                // diagonal
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of rows:\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns:\n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
