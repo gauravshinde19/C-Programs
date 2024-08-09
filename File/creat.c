fb6331cd44112508f0ea6cc4953727026d62ae16
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-08-09 17:21:02 +0530

    Create creat.c

---
+++ File/creat.c
@@ -0,0 +1,35 @@
+// Program to create file
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int CreateFile(char *fname)
+{
+    int iRet = 0;
+
+    iRet = fcreat(fname, 0777); // (Name of the file, Permission)
+
+    return iRet;
+}
+
+int main()
+{
+    char File_Name[30] = {'\0'};
+    int fd = 0;
+
+    printf("Enter the name of file that you want to create :\n");
+    scanf("%s", File_Name);
+
+    fd = CreateFile(File_Name);
+
+    if (fd == -1)
+    {
+        printf("Unable to create the file\n");
+    }
+    else
+    {
+        printf("File gets created successfully with fd %d\n", fd);
+    }
+
+    close(fd);
+}
